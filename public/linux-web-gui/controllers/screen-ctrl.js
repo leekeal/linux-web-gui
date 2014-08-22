4bd5b9016ee3836b2232f161e9df01a7fe8d2d35
accord <xiaogege.123@163.com>
2014-08-22 17:13:27 +0900

    添加了显示器功能

--- public/linux-web-gui/controllers/screen-ctrl.js
+++ public/linux-web-gui/controllers/screen-ctrl.js
@@ -6,10 +6,19 @@ linuxGui.controller('screenBodyCtrl',['$scope','$compile','$element',function($s
 }])
 
 
-linuxGui.controller('screenDashboardCtrl',['$scope','$compile',function($scope,$compile){
-	$scope.events.addInstance = function(){
-		var template = '<gui-instance></gui-instance>';
+linuxGui.controller('screenDashboardCtrl',['$scope','$compile','$element',function($scope,$compile,$element){
+	$scope.events.createGuiInstance = function(){
+		var template = '<div class="gui-instance"></div>';
 		var content = $compile(template)($scope);
 		$('.screen .screen-body').append(content);
+	}
+
+	$scope.dashboardSwitch = "screen-dashboard-close";
+	$scope.events.dashboardSwitch = function(){
+		if($scope.dashboardSwitch == "screen-dashboard-open"){
+			$scope.dashboardSwitch = "screen-dashboard-close";
+		}else{
+			$scope.dashboardSwitch = "screen-dashboard-open";
+		}
 	}
 }])
